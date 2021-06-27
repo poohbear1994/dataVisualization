0d64bb4c215a50011078894eebb0b11fcbdcb07f
卢见信 <lujianxin@lujianxindeMacBook-Pro.local>
2021-06-27 14:41:48 +0800

    vue-echarts销售大盘地图组件开发

--- datav-report-dev/src/router/index.js
+++ datav-report-dev/src/router/index.js
@@ -1,14 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 17:15:17
- * @LastEditTime: 2021-06-17 17:18:07
- * @LastEditors: your name
+ * @LastEditTime: 2021-06-26 13:31:35
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /Data visualization/datav-report-dev/src/router/index.js
  */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import BMap from '../views/BMap.vue'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,10 @@ const routes = [{
   path: '/',
   name: 'Home',
   component: Home
+}, {
+  path: '/bmap',
+  name: 'BMap',
+  component: BMap
 }]
 
 const router = new VueRouter({
