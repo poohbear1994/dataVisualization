71d3a17fd7aa831ccd8721b217d9e09b12b5e203
卢见信 <lujianxin@lujianxindeMacBook-Pro.local>
2021-06-28 19:02:10 +0800

    原生wordcloud词云学习

--- datav-report-dev/src/router/index.js
+++ datav-report-dev/src/router/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-17 17:15:17
- * @LastEditTime: 2021-06-28 16:37:05
+ * @LastEditTime: 2021-06-28 18:45:39
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /Data visualization/datav-report-dev/src/router/index.js
@@ -10,6 +10,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Liquidfill from '../views/Liquidfill.vue'
+import WordCloud from '../views/WordCloud.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,10 @@ const routes = [{
   path: '/liquidfill',
   name: 'Liquidfill',
   component: Liquidfill
+}, {
+  path: '/wordcloud',
+  name: 'WordCloud',
+  component: WordCloud
 }]
 
 const router = new VueRouter({
---
+++ datav-report-dev/src/views/WordCloud.vue
@@ -0,0 +1,86 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-06-28 18:44:39
+ * @LastEditTime: 2021-06-28 18:56:02
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /Data visualization/datav-report-dev/src/views/WordCloud.vue
+-->
+<template>
+  <div id="container"></div>
+</template>
+
+<script>
+import 'echarts-wordcloud'
+export default {
+  name: '',
+  components: {
+
+  },
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+// 生命周期 - 创建完成（访问当前this实例）
+  created () {
+
+  },
+// 生命周期 - 挂载完成（访问DOM元素）
+  mounted () {
+    const data = [{
+      name: '噗噗熊',
+      value: 100
+    }, {
+      name: '数据可视化',
+      value: 50
+    }, {
+      name: 'ljx',
+      value: 25
+    }]
+    const chart = this.$echarts.init(document.getElementById('container'))
+    chart.setOption({
+      series: [{
+        type: 'wordCloud',
+        data,
+        // 形状
+        shape: 'diamond',
+        // Global text style
+        textStyle: {
+          normal: {
+            fontFamily: 'sans-serif',
+            fontWeight: 'bold',
+            // Color can be a callback function or a color string
+            color: function () {
+                // Random color
+                return 'rgb(' + [
+                    Math.round(Math.random() * 160),
+                    Math.round(Math.random() * 160),
+                    Math.round(Math.random() * 160)
+                ].join(',') + ')'
+            }
+          }
+        },
+        emphasis: {
+          focus: 'self',
+          textStyle: {
+              shadowBlur: 10,
+              shadowColor: '#333'
+          }
+        }
+      }]
+    })
+  }
+}
+</script>
+
+<style lang='scss' scoped>
+/* @import url(); 引入css类 */
+#container {
+  width: 100%;
+  height: 100%;
+}
+</style>
