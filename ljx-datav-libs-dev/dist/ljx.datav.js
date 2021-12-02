ba144777238a86a2a6543cb60201c1a47b1d5165
卢见信 <lujianxin@lujianxindeMacBook-Pro.local>
2021-12-02 11:43:03 +0800

    tree-shaking机制

--- ljx-datav-libs-dev/dist/ljx.datav.js
+++ ljx-datav-libs-dev/dist/ljx.datav.js
@@ -4,26 +4,29 @@
   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ljxDataV = factory());
 })(this, (function () { 'use strict';
 
-  var a = Math.floor(Math.random() * 10);
-  var b = Math.floor(Math.random() * 100);
-  function random(base) {
-    if (base && base % 1 === 0) {
-      return Math.floor(Math.random() * base);
-    } else {
-      return 0;
-    }
+  /*
+   * @Author: your name
+   * @Date: 2021-12-02 11:16:35
+   * @LastEditTime: 2021-12-02 11:34:31
+   * @LastEditors: Please set LastEditors
+   * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+   * @FilePath: /ljx-datav-libs-dev/src/plugin.js
+   */
+
+  function random() {
+    console.log('random');
   }
 
   /*
    * @Author: your name
    * @Date: 2021-12-01 21:21:19
-   * @LastEditTime: 2021-12-02 01:32:55
+   * @LastEditTime: 2021-12-02 11:37:20
    * @LastEditors: Please set LastEditors
    * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
    * @FilePath: /ljx-datav-libs-dev/src/index.js
    */
 
-  console.log(random, a, b);
+  console.log(random());
 
   return random;
 
--- ljx-datav-libs-dev/src/index.js
+++ ljx-datav-libs-dev/src/index.js
@@ -1,14 +1,13 @@
 /*
  * @Author: your name
  * @Date: 2021-12-01 21:21:19
- * @LastEditTime: 2021-12-02 01:32:55
+ * @LastEditTime: 2021-12-02 11:37:20
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /ljx-datav-libs-dev/src/index.js
  */
+import { random } from './plugin'
 
-import { random, a, b } from 'sam-test-data'
-
-console.log(random, a, b)
+console.log(random())
 
 export default random
